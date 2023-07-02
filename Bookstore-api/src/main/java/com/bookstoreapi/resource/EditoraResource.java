311bbffcec27a4662afa16cfd668e60d528b05e6
Miguel Luiz de Albuquerque <miguelluizdealbuquerque@gmail.com>
2023-07-02 01:51:16 -0300

    Ajuste mapping
    Correção de mapeamento ambíguo.

--- Bookstore-api/src/main/java/com/bookstoreapi/resource/EditoraResource.java
+++ Bookstore-api/src/main/java/com/bookstoreapi/resource/EditoraResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/autores")
+@RequestMapping("/editoras")
 public class EditoraResource {
     private final EditoraService editoraService;
 
